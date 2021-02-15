a1d58bad54f07b50faf7592893c36d1f6c5303fa
jcconklin <jcconklin@users.noreply.github.com>
2021-02-15 09:58:36 -0500

    adding webpack for SAM

---
+++ functions/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: { hello: './src/handlers/hello-world.ts' },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+};
