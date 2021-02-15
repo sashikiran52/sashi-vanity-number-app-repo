c710d3b4b18af6e82eebda4fd1836594c931e60c
jcconklin <jcconklin@users.noreply.github.com>
2021-02-15 09:57:52 -0500

    project skeleton, SAM template converted to ts

---
+++ functions/src/tests/unit/test-handler.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import * as app from '../../handlers/hello-world';
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        const event: APIGatewayEvent = {
+            httpMethod: 'GET',
+        } as APIGatewayEvent;
+        const context: Context = {} as Context;
+        const result = await app.lambdaHandler(event, context);
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        const response = JSON.parse(result.body);
+
+        expect(response).to.be.an('object');
+        expect(response.message).to.be.equal('hello world');
+        // expect(response.location).to.be.an("string");
+    });
+});
